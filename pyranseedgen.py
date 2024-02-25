4c10c45fe4944a60070942cc87dd9a19e93448ce
KaylaByte <144823521+KaylaByte@users.noreply.github.com>
2024-02-25 13:36:15 +0000

    Code improvement + slight speed increase
    I simplified the code to make it not so messy! Simplifying the code didn't seem to have any significant effects on the speed (although it looks prettier now), but for some reason putting everything into a function sped up the code by a little bit? I read somewhere that the reason for this is because things inside a function are done locally, whereas things outside the function are global? I'm not entirely sure what it means, but I'm happy about the speed increase either way :)

--- pyranseedgen.py
+++ pyranseedgen.py
@@ -1,36 +1,36 @@
 import random
 import time
 
-seed = 0 # starts at 0, increases by 1 each loop
-breakit = 0 # amount of numbers that have worked successfully
-goto = 10 # length of brute-forced sequence
+def run():
+    seed = 0 # starts at 0, increases by 1 each loop
+    goto = 10 # length of brute-forced sequence
 
-start_time = time.perf_counter()
-print("Generating...")
+    start_time = time.perf_counter()
+    print("Generating...")
 
-while breakit < goto:
-    random.seed(seed)
-    for x in range(1, goto + 1, 1):
-        if random.randint(1,10) == x:
-            breakit += 1
-        else:
-            seed += 1
-            breakit = 0
-            break
+    seq_len = 1
+    while seq_len <= goto:
+        random.seed(seed)
+        for seq_len in range(1, goto + 2):
+            if random.randint(1,10) != seq_len:
+                break
+        seed += 1
 
-end_time = time.perf_counter()
+    end_time = time.perf_counter()
 
-print(f"Seed: {seed - 1}")
-print("Total gen time: {0:.2f} seconds.".format(end_time - start_time))
+    print(f"Seed: {seed - 1}")
+    print("Total gen time: {0:.2f} seconds.".format(end_time - start_time))
+
+run()
 
 # n: seed | avg brute force time on my PC
 # 1: 2 | instant
 # 2: 2 | instant
 # 3: 69 | instant
 # 4: 2387 | 0.01 seconds
-# 5: 146085 | 0.52 seconds
-# 6: 365871 | 1.30 seconds
-# 7: 2799290 | 9.91 seconds
-# 8: 401991108 | time unknown
-# 9: 1361757416 | time unknown
-# 10: 5234703478 | ~ 5 hours 12 minutes (whilst doing additional checks, so likely inaccurate)
+# 5: 146085 | 0.51 seconds
+# 6: 365871 | 1.26 seconds
+# 7: 2799290 | 9.60 seconds
+# 8: 401991108 | 1385.41 seconds (~ 23 minutes) 
+# 9: 1361757416 | 4705.57 seconds (~ 1 hour 18 minutes)
+# 10: 5234703478 | 18064.07 seconds (~ 5 hours 1 minute)
