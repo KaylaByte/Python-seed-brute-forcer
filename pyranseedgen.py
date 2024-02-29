b9fc496024ace8c444dbd5094b4ea49f243a45c4
KaylaByte <144823521+KaylaByte@users.noreply.github.com>
2024-02-29 04:02:03 +0000

    Code overhaul
    I remade the entire code to try squeeze as much performance from it as I possibly could. I found that instead of using a loop to generate numbers from 1 to n, I could use a list to create a custom order and the time impact from that would be negligible. I also remade the brute force loop to squeeze out as much time as I possibly could from it, including using a function from the itertools module that once again slightly improved speed. I then remembered hearing about multiprocessing, so I decided to use that to once again further increase the speed - leading to the greatest speed improvement thus far! I can think of minor changes that could be made to better this program, but I don't think it is worth it to add them as it leaves the scope of what I wanted to do with this small project :)

--- pyranseedgen.py
+++ pyranseedgen.py
@@ -1,36 +1,63 @@
 import random
 import time
+import itertools
+import multiprocessing as mp
 
-def run():
-    seed = 0 # starts at 0, increases by 1 each loop
-    goto = 10 # length of brute-forced sequence
+# CHANGE THESE VALUES HERE
+seq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # Sequence of numbers the program will attempt to generate a seed for
+lower = 1 # Lowest value random.randint() will generate
+upper = 10 # Highest value random.randint() will generate
+process_total = mp.cpu_count() # How many processes will be used, limited by cpu core count
 
+def brute_force(process_num):
+    global seq, process_total, upper, lower
     start_time = time.perf_counter()
-    print("Generating...")
 
-    seq_len = 1
-    while seq_len <= goto:
+    for seed in itertools.count(process_num, process_total): # Starts at 0, increases by 1 each loop
+        breakit = 0 # Used to break the loop once sequence is found
         random.seed(seed)
-        for seq_len in range(1, goto + 2):
-            if random.randint(1,10) != seq_len:
-                break
-        seed += 1
+        for seq_item in seq:
+            if random.randint(lower, upper) == seq_item:
+                breakit += 1
+                continue
+            break
+        if breakit == len(seq):
+            break
 
     end_time = time.perf_counter()
 
-    print(f"Seed: {seed - 1}")
+    print(f"Seed: {seed}")
     print("Total gen time: {0:.2f} seconds.".format(end_time - start_time))
+    return True
 
-run()
+print("Generating...")
+
+# Deals with the multiprocessing, significantly improves speed
+pool = mp.Pool(processes=process_total) 
+
+process_num = []
+for x in range(process_total):
+    process_num.append(x)
+
+processes = pool.imap_unordered(brute_force, process_num)
+pool.close()
+
+for process in processes:
+    if process:
+        pool.terminate()
+        break
+
+pool.join()
 
 # n: seed | avg brute force time on my PC
 # 1: 2 | instant
 # 2: 2 | instant
 # 3: 69 | instant
-# 4: 2387 | 0.01 seconds
-# 5: 146085 | 0.51 seconds
-# 6: 365871 | 1.26 seconds
-# 7: 2799290 | 9.60 seconds
-# 8: 401991108 | 1385.41 seconds (~ 23 minutes) 
-# 9: 1361757416 | 4705.57 seconds (~ 1 hour 18 minutes)
-# 10: 5234703478 | 18064.07 seconds (~ 5 hours 1 minute)
+# 4: 2387 | instant
+# 5: 146085 | 0.04 seconds
+# 6: 365871 | 0.13 seconds
+# 7: 2799290 | 0.78 seconds
+# 8: 401991108 | 114.11 seconds
+# 9: 1361757416 | 397.33 seconds
+# 10: 5234703478 | 1536.62 seconds
+# Pi: 33327794158 | 9484.80 seconds
